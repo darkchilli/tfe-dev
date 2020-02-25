b5cbdea5cfb9dc605d956d7e081261417a97d5fe
P795028 <ling.geng@nab.com,au>
2020-02-25 11:49:37 +1100

    added s3

--- main.tf
+++ main.tf
@@ -11,3 +11,13 @@ terraform {
 provider "aws" {
 	region = "ap-southeast-2"
 }
+
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
